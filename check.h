71db4555fd8e60cc0e21f71185139cf250f4987a
shingitai <shingitai@vivaldi.net>
2022-04-14 13:26:20 +0200

    update sleep time

--- check.h
+++ check.h
@@ -43,7 +43,7 @@ extern char bar_failure[FILENAME_MAX];
         strcat(bar_failure, "\033[1;31m#\033[30m");                                            \
         status = EXIT_FAILURE;                                                                 \
     }                                                                                          \
-    usleep(200000);                                                                            \
+    usleep(500000);                                                                            \
     if (status)                                                                                \
     {                                                                                          \
         immunity();                                                                            \
