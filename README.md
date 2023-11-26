34e06e1cb8409697885392796bb131d6c84906c1
Willy Micieli <micieli@vivaldi.net>
2023-11-26 10:10:54 +0100

    clean code

---
+++ README.md
@@ -0,0 +1,45 @@
+# Zuu
+
+A testing framework to all program written in rust.
+
+![build](https://raw.githubusercontent.com/taishingi/zuu/master/badges/build.svg)
+
+![check](https://raw.githubusercontent.com/taishingi/zuu/master/badges/check.svg)
+
+![doc](https://raw.githubusercontent.com/taishingi/zuu/master/badges/documentation.svg)
+
+![clippy](https://raw.githubusercontent.com/taishingi/zuu/master/badges/clippy.svg)
+
+![format](https://raw.githubusercontent.com/taishingi/zuu/master/badges/format.svg)
+
+![benchmark](https://raw.githubusercontent.com/taishingi/zuu/master/badges/benchmark.svg)
+
+![tests](https://raw.githubusercontent.com/taishingi/zuu/master/badges/tests.svg)
+
+![verify](https://raw.githubusercontent.com/taishingi/zuu/master/badges/verify.svg)
+
+![tree](https://raw.githubusercontent.com/taishingi/zuu/master/badges/tree.svg)
+
+## Installation
+
+```sh
+cargo install zuu
+```
+
+## Usage
+
+```sh
+zuu init
+```
+
+```sh
+git add .
+git commit -m "commit msg"
+```
+# Social links
+
+* [**Email**](mailto:micieli@vivaldi.net)
+* [**Donate**](https://www.paypal.com/donate/?hosted_button_id=D43VUM85PM724)
+* [**Github**](https://github.com/taishingi)
+* [**Discord**](https://discord.gg/U5gFCtfc)
+
---
+++ bin/zuu/main.rs
@@ -0,0 +1,130 @@
+use crossterm_cursor::TerminalCursor;
+use rsbadges::{Badge, Style};
+use std::env::current_dir;
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::Path;
+use std::process::{exit, Command};
+
+fn badge(name: &str, success: bool) {
+    if !Path::new("badges").is_dir() {
+        fs::create_dir("badges").expect("failed to create badges directory");
+    }
+    if success {
+        let badge = Badge {
+            label_text: name.to_string(),
+            msg_link: "".to_string(),
+            logo: "https://simpleicons.org/icons/rust.svg".to_string(),
+            embed_logo: true,
+            badge_link: "".to_string(),
+            badge_title: format!("{} status", name).to_string(),
+            label_title: "status".to_string(),
+            label_link: "".to_string(),
+            msg_text: "success".to_string(),
+            msg_title: "build status".to_string(),
+            label_color: "grey".to_string(),
+            msg_color: "green".to_string(),
+        };
+        let badge_style = Style::FlatSquare(badge);
+        let badge_svg = badge_style.generate_svg().unwrap();
+        rsbadges::save_svg(format!("{}/{}.svg", "badges", name).as_str(), &badge_svg).expect("");
+        assert!(std::path::Path::new(format!("{}/{}.svg", "badges", name).as_str()).is_file());
+    } else {
+        let badge = Badge {
+            label_text: name.to_string(),
+            msg_link: "".to_string(),
+            logo: "https://simpleicons.org/icons/rust.svg".to_string(),
+            embed_logo: true,
+            badge_link: "".to_string(),
+            badge_title: format!("{} status", name).to_string(),
+            label_title: "status".to_string(),
+            label_link: "".to_string(),
+            msg_text: "failure".to_string(),
+            msg_title: format!("{} status", name).to_string(),
+            label_color: "grey".to_string(),
+            msg_color: "red".to_string(),
+        };
+        let badge_style = Style::FlatSquare(badge);
+        let badge_svg = badge_style.generate_svg().unwrap();
+        rsbadges::save_svg(format!("{}/{}.svg", "badges", name).as_str(), &badge_svg).expect("");
+        assert!(std::path::Path::new(format!("{}/{}.svg", "badges", name).as_str()).is_file());
+    }
+}
+
+fn done(program: &str, arg: &str) -> bool {
+    let b = Command::new(program)
+        .arg(arg)
+        .current_dir(".")
+        .spawn()
+        .expect("")
+        .wait()
+        .expect("")
+        .success();
+    if b {
+        return true;
+    }
+    exit(1);
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 1 {
+        let cursor = TerminalCursor::new();
+        cursor.hide().expect("failed to hide cursor");
+
+        badge("verify", done("cargo", "verify-project"));
+        badge("build", done("cargo", "build"));
+        badge("clippy", done("cargo", "clippy"));
+        badge("check", done("cargo", "check"));
+        badge("benchmark", done("cargo", "bench"));
+        badge("format", done("cargo", "fmt"));
+        badge("documentation", done("cargo", "doc"));
+        badge("tests", done("cargo", "test"));
+        badge("tree", done("cargo", "tree"));
+        cursor.show().expect("failed to show cursor");
+        exit(0);
+    } else {
+        let cursor = TerminalCursor::new();
+        cursor.hide().expect("failed to hide cursor");
+        let arg: &String = args.get(1).expect("failed to get argument");
+        if arg.eq("init") {
+            let name = ".git/hooks/commit-msg";
+            if Path::new(name).is_file() {
+                println!(
+                    "Project already initialised at {:?}",
+                    current_dir().expect("failed to get current dir")
+                );
+                exit(0);
+            } else {
+                let mut x = File::create(name)
+                    .unwrap_or_else(|_| panic!("Failed to create the {} file", name));
+                x.write_all("#!/bin/bash\n\nzuu\n\nexit $?\n".as_bytes())
+                    .unwrap_or_else(|_| panic!("Failed to create the {} file", name));
+            }
+            assert!(Command::new("chmod")
+                .arg("+x")
+                .arg(".git/hooks/commit-msg")
+                .spawn()
+                .expect("failed to init git hook")
+                .wait()
+                .expect("")
+                .success());
+            cursor.show().expect("failed to restore cursor");
+            println!(
+                "Project initialised at {:?}",
+                current_dir().expect("failed to get current dir")
+            );
+            exit(0);
+        } else {
+            cursor.show().expect("failed to restore cursor");
+            help();
+        }
+    }
+}
+
+fn help() {
+    println!("zuu init : Initialize a new repository");
+    println!("zuu      : Run test in a initialized repository");
+    exit(0);
+}
