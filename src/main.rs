21e2c87e83291b5ee6e52bdd62f1cdd206145b95
Willy Micieli <micieli@vivaldi.net>
2024-01-03 18:34:41 +0100

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{env::args, fs, process::Command};
+
 const HOOK: &str = ".git/hooks/pre-commit";
+
 fn main() {
     if args().count() == 1 {
         Command::new("bash")
@@ -11,12 +13,12 @@ fn main() {
         let init = args.get(1).expect("msg");
         if init.eq(&"init") {
             Command::new("wget")
-                .arg("https://raw.githubusercontent.com/taishingi/zuu/master/pre-commit")
-                .current_dir(".")
+                .arg("https://raw.githubusercontent.com/taishingi/zuu/develop/pre-commit")
+                .current_dir("/tmp")
                 .spawn()
                 .expect("failed to get hook file");
-            fs::copy("pre-commit", HOOK).expect("Fail to copy the hook file");
-            fs::remove_file("pre-commit").expect("fail to remove hook file");
+            fs::copy("/tmp/pre-commit", HOOK).expect("Fail to copy the hook file");
+            fs::remove_file("/tmp/pre-commit").expect("fail to remove hook file");
         } else {
             println!("nad");
         }
@@ -30,9 +32,9 @@ mod test {
     fn init() {
         assert!(Command::new("cargo")
             .arg("run")
-            .arg("--example")
+            .arg("--bin")
             .arg("zuu")
-            .arg("-- --exoption init")
+            .arg("init")
             .current_dir(".")
             .spawn()
             .expect("msg")
